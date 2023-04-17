164119b1ef3e42c42a8579c644aa39bfaa7a0823
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-17 13:45:53 +0900

    Check Validation on signup form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,23 +7,43 @@ import {
   Input,
   VStack,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Center w="100%" p="20px">
       <VStack w="500px" spacing="10px">
-        <FormControl>
+        <FormControl isRequired isInvalid={!email.includes("@")}>
           <FormLabel>Email</FormLabel>
-          <Input type="email" data-testid="email-input" />
-          <FormErrorMessage>@ 포함</FormErrorMessage>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            data-testid="email-input"
+          />
+          {!email.includes("@") ? (
+            <FormErrorMessage>@ 포함</FormErrorMessage>
+          ) : null}
         </FormControl>
-        <FormControl>
+        <FormControl isRequired isInvalid={password.length < 8}>
           <FormLabel>Password</FormLabel>
-          <Input type="password" data-testid="password-input" />
-
-          <FormErrorMessage>8자 이상</FormErrorMessage>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            data-testid="password-input"
+          />
+          {password.length < 8 ? (
+            <FormErrorMessage>8자 이상</FormErrorMessage>
+          ) : null}
         </FormControl>
-        <Button type="submit" data-testid="signup-button">
+        <Button
+          type="submit"
+          data-testid="signup-button"
+          isDisabled={!email.includes("@") || password.length < 8}>
           Submit
         </Button>
       </VStack>
