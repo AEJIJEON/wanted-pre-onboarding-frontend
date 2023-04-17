e21abcaeca8a71845f6c4176d2775e363bf271b1
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-17 13:36:17 +0900

    Add Signup Form UI

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,32 @@
+import {
+  Button,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+
 export const Signup = () => {
-  return <div>signup page!</div>;
+  return (
+    <Center w="100%" p="20px">
+      <VStack w="500px" spacing="10px">
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" data-testid="email-input" />
+          <FormErrorMessage>@ 포함</FormErrorMessage>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input type="password" data-testid="password-input" />
+
+          <FormErrorMessage>8자 이상</FormErrorMessage>
+        </FormControl>
+        <Button type="submit" data-testid="signup-button">
+          Submit
+        </Button>
+      </VStack>
+    </Center>
+  );
 };
