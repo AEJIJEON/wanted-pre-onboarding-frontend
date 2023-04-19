6ed5ab0469fbba76f544e277418b3f731447f1cf
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-19 11:27:32 +0900

    Add signin page UI

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,3 +1,31 @@
+import { Button, Center, VStack } from "@chakra-ui/react";
+import { EmailInput } from "../components/EmailInput";
+import { PasswordInput } from "../components/PasswordInput";
+
 export const Signin = () => {
-  return <div>signin page!</div>;
+  return (
+    <Center w="100%" p="20px">
+      <VStack w="500px" spacing="10px">
+        <EmailInput
+          value={""}
+          isValid={true}
+          onChange={() => {}}
+          errorMessage={""}
+        />
+        <PasswordInput
+          value={""}
+          isValid={true}
+          onChange={() => {}}
+          errorMessage={""}
+        />
+        <Button
+          type="submit"
+          data-testid="signin-button"
+          onClick={() => {}}
+          isDisabled={true}>
+          Submit
+        </Button>
+      </VStack>
+    </Center>
+  );
 };
