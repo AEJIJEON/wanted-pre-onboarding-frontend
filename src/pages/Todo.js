7b4a6baee3f835e67a67b3078d58e9dd5cb84579
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-19 15:33:44 +0900

    Extract TodoItem component to a separate file

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,14 +1,13 @@
 import {
   Button,
   Center,
-  Checkbox,
   HStack,
   Input,
-  ListItem,
   UnorderedList,
   VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { TodoItem } from "../components/TodoItem";
 import { useTodos } from "../hooks/useTodos";
 
 export const Todo = () => {
@@ -45,81 +44,5 @@ export const Todo = () => {
         </UnorderedList>
       </VStack>
     </Center>
-  );
-};
-
-const TodoItem = ({ todo, updateTodo, deleteTodo }) => {
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [newTodo, setNewTodo] = useState(todo.todo);
-
-  const handleCheckBox = (checked) => {
-    updateTodo(todo.id, {
-      ...todo,
-      isCompleted: checked,
-    });
-  };
-
-  const handleSubmit = async () => {
-    await updateTodo(todo.id, {
-      ...todo,
-      todo: newTodo,
-    });
-    setIsEditMode(false);
-  };
-
-  const handleDelete = async () => {
-    await deleteTodo(todo.id);
-  };
-
-  return (
-    <ListItem>
-      {isEditMode ? (
-        <HStack>
-          <HStack flex="1">
-            <Checkbox
-              isChecked={todo.isCompleted}
-              onChange={(e) => handleCheckBox(e.target.checked)}
-            />
-            <Input
-              value={newTodo}
-              onChange={(e) => setNewTodo(e.target.value)}
-              data-testid="modify-input"
-            />
-          </HStack>
-          <HStack>
-            <Button onClick={handleSubmit} data-testid="submit-button">
-              제출
-            </Button>
-            <Button
-              onClick={() => {
-                setIsEditMode(false);
-                setNewTodo(todo.todo);
-              }}
-              data-testid="cancel-button">
-              취소
-            </Button>
-          </HStack>
-        </HStack>
-      ) : (
-        <HStack>
-          <Checkbox
-            flex="1"
-            isChecked={todo.isCompleted}
-            onChange={(e) => handleCheckBox(e.target.checked)}>
-            {todo.todo}
-          </Checkbox>
-          <HStack>
-            <Button
-              onClick={() => setIsEditMode(true)}
-              data-testid="modify-button">
-              수정
-            </Button>
-            <Button onClick={handleDelete} data-testid="delete-button">
-              삭제
-            </Button>
-          </HStack>
-        </HStack>
-      )}
-    </ListItem>
   );
 };
