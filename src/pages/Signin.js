dcd7a74d3c38b51f8d97789c53e956e0ae287fc6
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-19 11:29:59 +0900

    Handle signin input form

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,28 +1,39 @@
 import { Button, Center, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import { EmailInput } from "../components/EmailInput";
 import { PasswordInput } from "../components/PasswordInput";
+import { useFormValidation } from "../hooks/useFormValidation";
 
 export const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {
+    isFormValid,
+    email: emailValidation,
+    password: passwordValidation,
+  } = useFormValidation({ email, password });
+
   return (
     <Center w="100%" p="20px">
       <VStack w="500px" spacing="10px">
         <EmailInput
-          value={""}
-          isValid={true}
-          onChange={() => {}}
-          errorMessage={""}
+          value={email}
+          isValid={emailValidation.isValid}
+          onChange={(e) => setEmail(e.target.value)}
+          errorMessage={emailValidation.errorMessage}
         />
         <PasswordInput
-          value={""}
-          isValid={true}
-          onChange={() => {}}
-          errorMessage={""}
+          value={password}
+          isValid={passwordValidation.isValid}
+          onChange={(e) => setPassword(e.target.value)}
+          errorMessage={passwordValidation.errorMessage}
         />
         <Button
           type="submit"
           data-testid="signin-button"
           onClick={() => {}}
-          isDisabled={true}>
+          isDisabled={!isFormValid}>
           Submit
         </Button>
       </VStack>
