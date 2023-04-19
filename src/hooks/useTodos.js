3bb4778cc2479ea64b85191d49b83a9480aa8600
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-19 13:43:20 +0900

    Extract useTodos hook to a separate file

---
+++ src/hooks/useTodos.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { apiClient } from "../apis/apiClient";
+
+export const useTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = async () => {
+    try {
+      const todosData = await apiClient.getTodos();
+      setTodos(todosData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const refetchTodos = async () => {
+    try {
+      await fetchTodos();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const createTodo = async (todo) => {
+    try {
+      await apiClient.createTodo({ todo });
+      await refetchTodos();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return {
+    todos,
+    createTodo,
+  };
+};
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -8,8 +8,8 @@ import {
   UnorderedList,
   VStack,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { apiClient } from "../apis/apiClient";
+import { useState } from "react";
+import { useTodos } from "../hooks/useTodos";
 
 export const Todo = () => {
   const { todos, createTodo } = useTodos();
@@ -61,43 +61,4 @@ export const Todo = () => {
       </VStack>
     </Center>
   );
-};
-
-const useTodos = () => {
-  const [todos, setTodos] = useState([]);
-
-  const fetchTodos = async () => {
-    try {
-      const todosData = await apiClient.getTodos();
-      setTodos(todosData);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const refetchTodos = async () => {
-    try {
-      await fetchTodos();
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const createTodo = async (todo) => {
-    try {
-      await apiClient.createTodo({ todo });
-      await refetchTodos();
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  return {
-    todos,
-    createTodo,
-  };
 };
