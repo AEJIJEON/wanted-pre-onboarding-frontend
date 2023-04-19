b54feefa881420804785a7bf7493490bc40f1243
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-04-19 13:11:49 +0900

    Add todo page UI

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,3 +1,46 @@
+import {
+  Button,
+  Center,
+  Checkbox,
+  HStack,
+  Input,
+  ListItem,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
+
 export const Todo = () => {
-  return <div>todo page!</div>;
+  return (
+    <Center w="100%" p="20px">
+      <VStack spacing="30px">
+        <HStack w="full">
+          <Input data-testid="new-todo-input" />
+          <Button data-testid="new-todo-add-button">추가</Button>
+        </HStack>
+        <UnorderedList w="500px" spacing="10px">
+          <ListItem>
+            <HStack>
+              <Checkbox flex="1">TODO 1</Checkbox>
+              <HStack>
+                <Button data-testid="modify-button">수정</Button>
+                <Button data-testid="delete-button">삭제</Button>
+              </HStack>
+            </HStack>
+          </ListItem>
+          <ListItem>
+            <HStack>
+              <HStack flex="1">
+                <Checkbox />
+                <Input data-testid="modify-input" />
+              </HStack>
+              <HStack>
+                <Button data-testid="submit-button">제출</Button>
+                <Button data-testid="cancel-button">취소</Button>
+              </HStack>
+            </HStack>
+          </ListItem>
+        </UnorderedList>
+      </VStack>
+    </Center>
+  );
 };
